f0366399aef9a211f5a0b9e8c90b4ddcc1eb1666
OlacodesLA <zaccheausjide@gmail.com>
2023-06-12 16:46:55 +0100

    fix

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html className="font-dm">
+    <Html className="font-dm n">
       <Head>
         <link
           rel="apple-touch-icon"
